b0ad4a6f146f60010a06887f54eabca96ec093ad
Rifatx990 <158224402+Rifatx990@users.noreply.github.com>
2025-12-22 00:06:35 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
+  server: { 
+    host: '0.0.0.0',
+    port: 5000,
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:3001',
